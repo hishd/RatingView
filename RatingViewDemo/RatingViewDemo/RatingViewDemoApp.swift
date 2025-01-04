57b2baffeb7b80105d41f791712012560e857462
Hishara Dilshan <hisharadilshan3@gmail.com>
2025-01-04 08:40:38 +0000

    Initial Commit

---
+++ RatingViewDemo/RatingViewDemo/RatingViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RatingViewDemoApp.swift
+//  RatingViewDemo
+//
+//  Created by Hishara Dilshan on 04/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct RatingViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
