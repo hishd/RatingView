0ec1c83e32d1b881606d0d8930a2a113105cc68e
Hishara Dilshan <hisharadilshan3@gmail.com>
2025-01-08 20:46:00 +0000

    Extracted view components into properties

--- RatingViewDemo/RatingViewDemo/Rating View/RatingView.swift
+++ RatingViewDemo/RatingViewDemo/Rating View/RatingView.swift
@@ -30,118 +30,29 @@ public struct RatingView: View {
             ScrollView(showsIndicators: false) {
                 
                 VStack {
-                    Text(titleText)
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.black)
-                        .multilineTextAlignment(.center)
-                        .lineLimit(2)
-                        .padding(.top, 20)
+                    titleView
                     
-                    VStack {
-                        HStack(spacing: 20) {
-                            EyesExpressionView(lidValue: value)
-                            EyesExpressionView(lidValue: value)
-                        }
-                        .padding(.top, 50 + 30) //Path curve height of eyes = 50
-                        .padding(.bottom, ((1 - value) * 80))
-                        
-                        MouthExpressionShape(value: value)
-                            .stroke(.black, lineWidth: 3)
-                    }.frame(height: 320)
+                    expressionView
+                        .frame(height: 320)
                     
+                    commentTitleView
                     
-                    Text(commentTitle)
-                        .font(.largeTitle)
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.black)
-                    
-                    ZStack(alignment: .leading) {
-                        Color.black
-                            .frame(height: 2)
-                        
-                        Image(systemName: "arrow.right")
-                            .foregroundStyle(.white)
-                            .frame(width: 50, height: 40)
-                            .background(.black)
-                            .clipShape(.rect(cornerRadius: 10))
-                            .offset(x: value * (width - 90))
-                            .gesture(DragGesture().onChanged({ drag in
-                                let maxWidth = width - 90
-                                let dragValue = drag.location.x - 30
-                                if dragValue > 0 && dragValue < maxWidth {
-                                    withAnimation {
-                                        self.isTextFocused = false
-                                        self.value = dragValue / maxWidth
-                                    }
-                                }
-                            }))
-                        
-                    }.padding(.horizontal, 20)
+                    sliderView(of: width)
+                        .padding(.horizontal, 20)
                     
                     if enableWrittenFeedback {
-                        ZStack(alignment: .topLeading) {
-                            if #available(iOS 16.0, *) {
-                                TextEditor(text: $text)
-                                    .background(.white)
-                                    .foregroundStyle(.black)
-                                    .frame(height: 100)
-                                    .padding(.horizontal, 5)
-                                    .focused($isTextFocused)
-                                    .scrollContentBackground(.hidden)
-                            } else {
-                                List {
-                                    TextEditor(text: $text)
-                                        .background(.white)
-                                        .foregroundStyle(.black)
-                                        .frame(height: 100)
-                                        .focused($isTextFocused)
-                                        .padding(.horizontal, 5)
-                                }
-                            }
-                            
-                            Text("Write feedback")
-                                .foregroundStyle(.gray)
-                                .padding(.leading, 10)
-                                .padding(.top, 10)
-                                .opacity(text.count > 0 ? 0 : 1)
-                        }
-                        .background(.white)
-                        .clipShape(.rect(cornerRadius: 10))
-                        .padding(.vertical)
-                        .padding(.horizontal, 20)
+                        feedbackTextField
                     }
                     
                     
-                    Button {
-                        onSubmit(value, text)
-                    } label: {
-                        Text("Submit")
-                            .fontWeight(.medium)
-                            .foregroundStyle(.white)
-                            .frame(width: 100, height: 40)
-                            .background(.black)
-                            .clipShape(.rect(cornerRadius: 5))
-                            .padding()
-                    }
+                    submitButton
                 }
                 .frame(maxHeight: .infinity, alignment: .top)
             }
             .background {
-                let color = switch value {
-                case 0...0.2:
-                    Color.red
-                case 0.2...0.5:
-                    Color.orange
-                case 0.5...0.8:
-                    Color.yellow
-                default:
-                    Color.green
-                }
-                
-                return color
-                    .ignoresSafeArea(.all)
-                    .animation(.easeInOut, value: value)
+                backgroundColor
+                .ignoresSafeArea(.all)
+                .animation(.easeInOut, value: value)
             }
             .onTapGesture {
                 isTextFocused = false
@@ -162,6 +73,127 @@ extension RatingView {
             "GOOD"
         default:
             "EXCELLENT"
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch value {
+        case 0...0.2:
+            Color.red
+        case 0.2...0.5:
+            Color.orange
+        case 0.5...0.8:
+            Color.yellow
+        default:
+            Color.green
+        }
+    }
+}
+
+extension RatingView {
+    
+    var titleView: some View {
+        Text(titleText)
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundStyle(.black)
+            .multilineTextAlignment(.center)
+            .lineLimit(2)
+            .padding(.top, 20)
+    }
+    
+    var expressionView: some View {
+        VStack {
+            HStack(spacing: 20) {
+                EyesExpressionView(lidValue: value)
+                EyesExpressionView(lidValue: value)
+            }
+            .padding(.top, 50 + 30) //Path curve height of eyes = 50
+            .padding(.bottom, ((1 - value) * 80))
+            
+            MouthExpressionShape(value: value)
+                .stroke(.black, lineWidth: 3)
+        }
+    }
+    
+    var commentTitleView: some View {
+        Text(commentTitle)
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .foregroundStyle(.black)
+    }
+    
+    var feedbackTextField: some View {
+        ZStack(alignment: .topLeading) {
+            if #available(iOS 16.0, *) {
+                TextEditor(text: $text)
+                    .background(.white)
+                    .foregroundStyle(.black)
+                    .frame(height: 100)
+                    .padding(.horizontal, 5)
+                    .focused($isTextFocused)
+                    .scrollContentBackground(.hidden)
+            } else {
+                List {
+                    TextEditor(text: $text)
+                        .background(.white)
+                        .foregroundStyle(.black)
+                        .frame(height: 100)
+                        .focused($isTextFocused)
+                        .padding(.horizontal, 5)
+                }
+            }
+            
+            Text("Write feedback")
+                .foregroundStyle(.gray)
+                .padding(.leading, 10)
+                .padding(.top, 10)
+                .opacity(text.count > 0 ? 0 : 1)
+        }
+        .background(.white)
+        .clipShape(.rect(cornerRadius: 10))
+        .padding(.vertical)
+        .padding(.horizontal, 20)
+    }
+    
+    var submitButton: some View {
+        Button {
+            onSubmit(value, text)
+        } label: {
+            Text("Submit")
+                .fontWeight(.medium)
+                .foregroundStyle(.white)
+                .frame(width: 100, height: 40)
+                .background(.black)
+                .clipShape(.rect(cornerRadius: 5))
+                .padding()
+        }
+    }
+}
+
+extension RatingView {
+    func sliderView(of width: CGFloat) -> some View {
+        ZStack(alignment: .leading) {
+            Color.black
+                .frame(height: 2)
+            
+            Image(systemName: "arrow.right")
+                .foregroundStyle(.white)
+                .frame(width: 50, height: 40)
+                .background(.black)
+                .clipShape(.rect(cornerRadius: 10))
+                .offset(x: value * (width - 90))
+                .gesture(DragGesture().onChanged({ drag in
+                    let maxWidth = width - 90
+                    let dragValue = drag.location.x - 30
+                    if dragValue > 0 && dragValue < maxWidth {
+                        withAnimation {
+                            self.isTextFocused = false
+                            self.value = dragValue / maxWidth
+                        }
+                    }
+                }))
+            
         }
     }
 }
